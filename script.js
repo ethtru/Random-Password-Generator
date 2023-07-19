3531118f2282688b62e09b4d3b1d2f1edd0a17b8
Eth <ethtru@gmail.com>
2023-07-18 23:11:29 -0400

    completed generate password and prompt response functinos

--- script.js
+++ script.js
@@ -59,7 +59,6 @@ var uppercaseChar = [
 var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var specialChar = [
-  " ",
   "!",
   "#",
   "$",
@@ -92,7 +91,7 @@ var specialChar = [
 ];
 
 var selectionArray = [];
-var passwordLength = [];
+var passwordLength;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -106,7 +105,15 @@ function writePassword() {
 }
 
 function generatePassword() {
-
+  var userChoice = promptResponse();
+  console.log(userChoice)
+  var randomArray = [];
+  for (var i = 0; i < passwordLength; i++) {
+    const randomElement =
+      userChoice[Math.floor(Math.random() * userChoice.length)];
+    randomArray.push(randomElement);
+  }
+  return randomArray.join("");
 }
 
 // Add event listener to generate button
@@ -114,42 +121,52 @@ generateBtn.addEventListener("click", writePassword);
 
 function promptResponse() {
   //length of password is first prompt. must be between 8 and 128 characters long
-  var passwordLength = parseInt(window.prompt(
-    "Please select a password length between 8 and 128 characters."
-  ));
+  passwordLength = parseInt(
+    window.prompt(
+      "Please select a password length between 8 and 128 characters."
+    )
+  );
   if (passwordLength < 8) {
     alert("Length must be between 8 and 128 characters!");
-    return;
+    return [];
   }
   if (passwordLength > 128) {
     alert("Length must be between 8 and 128 characters!");
+    return [];
   }
-  if (passwordLength = isNaN) {
-    alert("Value must be a number.")
+  if (passwordLength === isNaN) {
+    alert("Value must be a number.");
+    return [];
   }
 
-  if (window.confirm("Use lower case characters?")); {
+  if (window.confirm("Use lower case characters?"));
+  {
     selectionArray = selectionArray.concat(lowercaseChar);
   }
 
- if (window.confirm("Use upper case characters?")); {
-  selectionArray = selectionArray.concat(uppercaseChar);
- }
+  if (window.confirm("Use upper case characters?"));
+  {
+    selectionArray = selectionArray.concat(uppercaseChar);
+  }
 
- if (window.confirm("Use numeric characters?")); {
-  selectionArray = selectionArray.concat(numericChar);
- }
+  if (window.confirm("Use numeric characters?"));
+  {
+    selectionArray = selectionArray.concat(numericChar);
+  }
 
-  if (window.confirm("Use special characters?")); {
+  if (window.confirm("Use special characters?"));
+  {
     selectionArray = selectionArray.concat(specialChar);
   }
 
-  return;
+  return selectionArray;
 }
+
+//will need some kind of "for" loop for running through each batch of variables
 
 //prompt 2 is lowercase characters
 //prompt 3 is upper case characters
 //prompt 4 is numeric characters
-//prompt 5 is special characters
+//prompt 5 is special charactersse
 //we need to make sure at least one of the prompts was selected
 //next, the password is generated
